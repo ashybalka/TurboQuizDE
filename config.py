92f29cbd1fa87d81c1e3f3be924dd8e573cc14ae
Artyom <40268012+ashybalka@users.noreply.github.com>
2026-01-11 15:04:34 +0100

    delete twitch chat

--- config.py
+++ config.py
@@ -1,13 +1,19 @@
 import os
 
-# Central configuration for chat integrations
+#Twitch credentials
 IRC_SERVER = "irc.chat.twitch.tv"
 IRC_PORT = 6667
 IRC_NICK = os.environ.get("IRC_NICK", "turboquizde")
-IRC_TOKEN = os.environ.get("IRC_TOKEN", "")  # Токен берем из переменных окружения
+IRC_TOKEN = os.environ.get("IRC_TOKEN", "")
 IRC_CHANNEL = os.environ.get("IRC_CHANNEL", "#turboquizde")
+
+#YouTube credentials
 YOUTUBE_VIDEO_ID = os.environ.get("YOUTUBE_VIDEO_ID", "")
 YOUTUBE_CHANNEL_ID = os.environ.get("YOUTUBE_CHANNEL_ID", "")
+
+#TikTok credentials
 TTS_DEVICE_NAME = os.environ.get("TTS_DEVICE_NAME", "")
 TIKTOK_USERNAME = os.environ.get("TIKTOK_USERNAME", "")
-TIKTOK_SIGN_API_KEY = os.environ.get("TIKTOK_SIGN_API_KEY")
+
+# General settings
+BACKGROUND_MUSIC_URL = os.environ.get("BACKGROUND_MUSIC_URL", "")
